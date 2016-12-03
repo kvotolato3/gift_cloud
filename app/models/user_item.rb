e28bf1c2346c559b807439c9c1ced10184b8215a
Kathleen Votolato <kvotolato3@gmail.com>
2016-12-02 22:07:03 -0500

    Added validation for presence of name on user_item, and error msgs

--- app/models/user_item.rb
+++ app/models/user_item.rb
@@ -2,4 +2,6 @@ class UserItem < ApplicationRecord
   belongs_to :user
   has_many :occasion_user_items
   has_many :occasions, through: :occasion_user_items
+
+  validates :name, presence: true
 end
