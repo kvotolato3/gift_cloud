dd7501213be8a841c4972cc656bad59687e70d07
Kathleen Votolato <kvotolato3@gmail.com>
2017-03-29 12:12:13 -0400

    Add route for creating a claim

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   resources :users, except: [:create, :destroy, :edit, :new, :index, :show, :update] do
     resources :user_items, only: [:new, :create, :index]
+    resources :claims, only: [:create]
   end
 
   resources :occasions, only: [:new, :create, :show]
